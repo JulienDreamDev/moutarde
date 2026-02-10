7f4f2b9cf953243a0942831605ad8f527c96e5cf
Julien <pro.juliendacunha@gmail.com>
2026-02-10 14:50:26 +0100

    docs: swagger endpoints added to backend

--- moutarde-back/moutarde-back/Program.cs
+++ moutarde-back/moutarde-back/Program.cs
@@ -7,17 +7,25 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+            {
+                Title =  "〽️ Moutarde - API",
+                Version = "v1",
+                Description = "Mini Social Network"
+            });
+        });
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
         else // Only use HTTPS in prod.
         {
