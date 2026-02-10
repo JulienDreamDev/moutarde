2ea43dac3ac638222cc069cdff151f3fedd3b797
Julien <pro.juliendacunha@gmail.com>
2026-02-10 18:46:21 +0100

    feature: user entity mapping to dbcontext

--- moutarde-back/moutarde-back/Infrastructure/Data/MoutardeDbContext.cs
+++ moutarde-back/moutarde-back/Infrastructure/Data/MoutardeDbContext.cs
@@ -1,7 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using moutarde_back.Entities;
 
 namespace moutarde_back.Infrastructure.Data;
 
 public class MoutardeDbContext(DbContextOptions<MoutardeDbContext> options) : DbContext(options)
 {
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.Property(u => u.Username).IsRequired().HasMaxLength(20);
+            entity.Property(u => u.Email).IsRequired().HasMaxLength(255);
+            entity.HasIndex(u => u.Email).IsUnique();
+            entity.Property(u => u.PasswordHash).IsRequired();
+            entity.Property(u => u.CreatedAt).IsRequired();
+        });
+    }
 }
