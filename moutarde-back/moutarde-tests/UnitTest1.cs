512b10c9ce19e211ae9662622a5ee9f42be4564e
Julien <pro.juliendacunha@gmail.com>
2026-02-11 14:50:34 +0100

    test: bcrypt password hasher tested

--- moutarde-back/moutarde-tests/UnitTest1.cs
+++ moutarde-back/moutarde-tests/UnitTest1.cs
@@ -1,9 +1,37 @@
-﻿namespace moutarde_tests;
+﻿using moutarde_back.Infrastructure.Security;
+
+namespace moutarde_tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void PasswordHasBeenHashedTest()
     {
+        IPasswordHasher passwordHasher = new BCryptPasswordHasher();
+        string password = "password123";
+        string hash = passwordHasher.Hash(password);
+
+        Assert.NotEqual(password, hash);
+    }
+    
+    [Fact]
+    public void PasswordVerificationTest()
+    {
+        IPasswordHasher passwordHasher = new BCryptPasswordHasher();
+        string password = "password123";
+        string hash = passwordHasher.Hash(password);
+
+        Assert.True(passwordHasher.Verify(password, hash));
+    }
+    
+    [Fact]
+    public void PasswordVerificationFailedTest()
+    {
+        IPasswordHasher passwordHasher = new BCryptPasswordHasher();
+        string password = "password123";
+        string wrongPassword = "wrongpassword";
+        string hash = passwordHasher.Hash(password);
+
+        Assert.False(passwordHasher.Verify(wrongPassword, hash));
     }
 }
