ecbb84ba234e1fa21f92d88078f03ac89fe3f36f
Julien <pro.juliendacunha@gmail.com>
2026-02-13 14:41:28 +0100

    docs: ITokenService docs updated

--- moutarde-back/moutarde-back/Infrastructure/Security/ITokenService.cs
+++ moutarde-back/moutarde-back/Infrastructure/Security/ITokenService.cs
@@ -7,7 +7,7 @@ public interface ITokenService
     /// <summary>
     /// Generates a JWT token for the given user.
     /// </summary>
-    /// <param name="user">The user data</param>
-    /// <returns>The JWT token</returns>
+    /// <param name="user">The user data used to generate the token</param>
+    /// <returns>The user's JWT token</returns>
     public string GenerateToken(User user);
 }
