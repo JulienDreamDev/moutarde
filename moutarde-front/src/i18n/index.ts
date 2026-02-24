d24ef4f1d8dadf218fe362ccd024fd15790485e5
Julien <pro.juliendacunha@gmail.com>
2026-02-24 12:54:20 +0100

    feat: toggling between french and english done

--- moutarde-front/src/i18n/index.ts
+++ moutarde-front/src/i18n/index.ts
@@ -3,12 +3,16 @@ import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
 import fr from "./locales/fr.json";
 
+const savedLanguage = localStorage.getItem("language");
+const navigatorLanguage = navigator.language.split("-")[0];
+const defaultLanguage = savedLanguage || navigatorLanguage || "en";
+
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     fr: { translation: fr },
   },
-  defaultNS: "translation",
+  lng: defaultLanguage,
   fallbackLng: "en",
   interpolation: {
     escapeValue: false,
